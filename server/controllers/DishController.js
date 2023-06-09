0552883361982b16c935780648a95cb82992f744
relt <haxrelt@gmail.com>
2023-06-09 20:27:39 +0300

    route controllers

---
+++ server/controllers/DishController.js
@@ -0,0 +1,11 @@
+class DishController {
+    async create(request, response) {
+
+    }
+
+    async getAll(request, response) {
+
+    }
+}
+
+module.exports = new DishController()
---
+++ server/controllers/UserController.js
@@ -0,0 +1,15 @@
+class UserController {
+    async signup(request, response) {
+
+    }
+
+    async signin(request, response) {
+
+    }
+
+    async auth(request, response) {
+
+    }
+}
+
+module.exports = new UserController()
